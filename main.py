a2afba8caf417751494e0d9f5a6fd269af033e74
danielpadkins <danielpadk@gmail.com>
2024-02-11 16:09:47 -0500

    scaffolding

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ parser = argparse.ArgumentParser(description='Configure log level for the applic
 parser.add_argument('-l', '--log_level', type=str, help='Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)', default='INFO')
 
 def run():
+
+    # This is a monolithic run function that performs all steps of the pipeline
+    # In the future, we will break this down into smaller functions that interact via a message queue
+
+    # Fetch document from source URL 
+    # - This might involve taxonomy/process discovery/function writing
+    # - Or just executing the function if we already know how 
+
+    # Extract data from document 
+
+    # Extract metadata from document (e.g. title, author, date, topics.)
+
+    # Perform side effects on document (e.g. create/update other entities like people, cases)
+    # - This involves sequences of actions like lookups to see which entities we have already 
+    # - Requires knowledge of entire schema 
+
     pass
 
 if __name__ == "__main__":
